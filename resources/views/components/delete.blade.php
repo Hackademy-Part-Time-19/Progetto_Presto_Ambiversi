a8be13f7825d42c50744f5774e4fde5a4513e48e
Samuele <samuelefasciglione@gmail.com>
2024-03-26 21:18:44 +0100

    prova

--- resources/views/components/delete.blade.php
+++
@@ -1,3 +0,0 @@
-@if (session()->has('delete'))
-<h2 class="alert alert-danger">{{session('delete')}}</h2>
-@endif
--- resources/views/components/success.blade.php
+++
@@ -1,3 +0,0 @@
-@if (session()->has('success'))
-<h2 class="alert alert-success">{{session('success')}}</h2>
-@endif
