cd42e9e94dc17b24b4d91254c4a45a6415c25d6b
Samuele <samuelefasciglione@gmail.com>
2024-04-08 19:10:56 +0200

    lang

---
+++ resources/views/components/locale.blade.php
@@ -0,0 +1,7 @@
+<form action="{{route('set_language_locale',$lang)}}" method="POST">
+@csrf
+<button type="submit" class="nav-link" style="background-color: transparent; border:none">
+    <x-flag-country-br />
+</button>
+
+</form>
