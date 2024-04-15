257f2d6b2c010696bcb0e4c022916236c75f37ef
antvill22 <117175629+antvill22@users.noreply.github.com>
2024-04-16 01:34:33 +0200

    Update RemoveFaces.php

--- app/Jobs/RemoveFaces.php
+++ app/Jobs/RemoveFaces.php
@@ -4,20 +4,15 @@ namespace App\Jobs;
 
 use App\Models\Image;
 use Illuminate\Bus\Queueable;
-<<<<<<< HEAD
 use Intervention\Image\ImageManager;
-=======
->>>>>>> 49958fee1329a06a8909b2f9928284b6f25d71d9
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Google\Cloud\Vision\V1\ImageAnnotatorClient;
-<<<<<<< HEAD
-=======
 use Spatie\Image\Image as SpatieImage;
 use Spatie\Image\Manipulations;
->>>>>>> 49958fee1329a06a8909b2f9928284b6f25d71d9
+
 
 
 class RemoveFaces implements ShouldQueue
@@ -58,25 +53,7 @@ class RemoveFaces implements ShouldQueue
             }
             $w = $bounds[2][0] - $bounds[0][0];
             $h = $bounds[2][1] - $bounds[0][1];
-<<<<<<< HEAD
-            $watermarkPath = base_path('resources/img/Trollface_non-free.png');
-            $watermark = imagecreatefrompng($watermarkPath);
 
-            // Carica l'immagine originale
-            $image = imagecreatefromstring(file_get_contents($srcPath));
-
-            // Inserisci il watermark nell'immagine originale
-            imagecopyresampled($image, $watermark, $bounds[0][0], $bounds[0][1], 0, 0, $w, $h, imagesx($watermark), imagesy($watermark));
-
-            // Salva l'immagine manipolata
-            imagepng($image, $srcPath);
-
-            // Libera la memoria dalle immagini create
-            imagedestroy($image);
-            imagedestroy($watermark);
-        }
-
-=======
             $image = SpatieImage::load($srcPath);
             $image->watermark(base_path('storage/app/public/images/pixel.png'))
                 ->watermarkPosition('top-left')
@@ -86,7 +63,6 @@ class RemoveFaces implements ShouldQueue
                 ->watermarkFit(Manipulations::FIT_STRETCH);
             $image->save($srcPath);
         }
->>>>>>> 49958fee1329a06a8909b2f9928284b6f25d71d9
         $imageAnnotator->close();
     }
 }
